d29e805ff849926d0dc2af86bf0ac8c921d36af2
Bagus Andrian <babagusandrian@gmail.com>
2022-09-27 19:54:50 +0700

    Update main.go
    change method

--- main.go
+++ main.go
@@ -8,34 +8,29 @@ import (
 
 	"golang.org/x/sync/singleflight"
 )
-
-var requestGroup singleflight.Group
-
-/*
-how to test:
-ab -n 100 -c 100 localhost:8080/normal
-*/
+// ab -n 100 -c 100  -t 10 http://localhost:8080/singleflight
 func main() {
+	var requestGroup singleflight.Group
 	// localhost:8080/normal
 	http.HandleFunc("/normal", func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		// call function githubStatus()
-		status, err := githubStatus()
+		// call function externalCall()
+		status, err := externalCall()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		// print processing time and status
-		log.Printf("/github handler requst: processing time: %+v | status %q", time.Since(start), status)
+		log.Printf("/normal handler requst: processing time: %+v | status %q", time.Since(start), status)
 
-		fmt.Fprintf(w, "GitHub Status: %q", status)
+		fmt.Fprintf(w, "response Status: %q", status)
 	})
 
 	http.HandleFunc("/singleflight", func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		v, err, shared := requestGroup.Do("github", func() (interface{}, error) {
-			// call function githubStatus()
-			return githubStatus()
+		v, err, shared := requestGroup.Do("apply_sf_key", func() (interface{}, error) {
+			// call function external
+			return externalCall()
 		})
 
 		// Check the error, as before.
@@ -47,25 +42,26 @@ func main() {
 		status := v.(string)
 
 		// print processing time and status
-		log.Printf("/github handler requst: processing time: %+v | status %q | shared: %t", time.Since(start), status, shared)
-
-		fmt.Fprintf(w, "GitHub Status: %q", status)
+		if shared {
+			log.Printf("/single flight handler requst: processing time: %+v | status %q | shared: %t", time.Since(start), status, shared)
+		}
+		fmt.Fprintf(w, "response Status: %q", status)
 	})
 	log.Println("running server on port :8080")
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
 
-func githubStatus() (string, error) {
-	time.Sleep(1 * time.Second)
-	log.Println("call githubStatus")
-	resp, err := http.Get("https://api.github.com")
+func externalCall() (string, error) {
+	time.Sleep(300 * time.Millisecond)
+	log.Println("call external")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("github response: %s", resp.Status)
+		return "", fmt.Errorf("response externalCall: %s", resp.Status)
 	}
-
+	log.Println("response externalCall", resp.Status)
 	return resp.Status, err
 }
